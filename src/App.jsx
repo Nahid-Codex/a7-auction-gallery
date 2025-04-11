82d6d3955234fc885298a4121daa47f95ac7e27e
Nahid Codex <Web.nahidhasan@gmail.com>
2025-04-11 16:30:38 +0600

    project setup done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar"
+import "./index.css"
+function App() {
+
+
+  return (
+    <div className="max-w-[1380px] mx-auto">
+
+      <Navbar></Navbar>
+    </div>
+  )
+}
+
+export default App
