39b810c04e0b5902a2600d5611c974d47546deb3
Nahid Codex <Web.nahidhasan@gmail.com>
2025-04-11 18:01:33 +0600

    fake json data created

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
+import { useEffect, useState } from "react";
 import Banner from "./components/Banner"
 import Footer from "./components/Footer"
 import Navbar from "./components/Navbar"
 import "./index.css"
 function App() {
+  const [auctions, setAuctions] = useState([]);
 
+  useEffect(() => {
+    fetch("/auctions.json")
+      .then((res) => res.json())
+      .then((data) => setAuctions(data));
+  }, []);
+
+console.log(auctions);
 
   return (
     <div className="max-w-[1380px] mx-auto">
 
-     
+
       <Navbar></Navbar>
       <Banner></Banner>
       <Footer></Footer>
-   
+
     </div>
   )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,13 +3,13 @@ import logo from '../assets/logo.svg'
 const Footer = () => {
     return (
         <div className='flex flex-col items-center justify-center bg-white py-30'>
-            <img src={logo} alt="" srcset="" />
+            <img src={logo} alt=""  />
             <div className='flex  font-sora  gap-4 mt-3 text-[#000] text-[22px] font-normal'>
                 <span>Bid.</span>
                 <span>Win.</span>
                 <span>Own.</span>
             </div>
-            <ul className="flex gap-8 font-poppins font-normal justify-center text[#000] mt-6 text-lg ">
+            <ul className="flex gap-8 font-poppins font-normal justify-center text-[#000] mt-6 text-lg ">
                 <li><a href="#">Home</a></li>
                 <li><a href="#">Auctions</a></li>
                 <li><a href="#">Categories</a></li>
